fe35ea78ccdb00a7adad5d2a5712bc8454e4cbb9
talrez11 <tal@way2cu.com>
2015-04-19 14:04:37 +0300

    Added dialog function to main.js

--- site/scripts/main.js
+++ site/scripts/main.js
@@ -48,7 +48,35 @@ Site.is_mobile = function() {
 /**
  * Function called when document and images have been completely loaded.
  */
+
+ function dialog() {
+
+	// calling the dialog.
+	var video_dialog = new Dialog();
+
+	//Modefy the dialog.
+
+	video_dialog.setTitle(language_handler.getText(null, 'dialog_video_title'));
+	video_dialog.setSize(640, 330);
+
+	$('a.youtube').not('.mobile').click(function(event) {
+
+		// prevent link from working.
+		event.preventDefault();
+
+		// set content from URL and show it.
+		video_dialog.setContentFromURL($(this).attr('href'));
+		video_dialog.showWhenReady();
+	});
+}
+
 Site.on_load = function() {
+
+	/*
+	*** Calling the function dialog.
+	*/
+
+	dialog();
 };
 
 
